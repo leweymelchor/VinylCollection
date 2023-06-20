d74a564b4413b9ab8b375bf57a1ad5849b8e5580
Lewey <dlmelchor12@gmail.com>
2023-06-20 13:28:16 -0700

    docker work

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,16 @@ LABEL maintainer="vinylcollection.com"
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
+COPY ./vinyl_collection /vinyl_collection
+
+WORKDIR /vinyl_collection
+EXPOSE 8000
+
+RUN python -m venv /py && \
+    /py/bin/pip install --upgrade pip && \
+    /py/bin/pip install -r /requirements.txt && \
+    adduser --disabled-password --no-create-home app
+
+ENV PATH="/bin:$PATH"
+
+USER app
--- vinyl_collection/settings.py
+++ vinyl_collection/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ii#n*gcw_(ps7xacbuq&+hp0@#n&(_-jy69z7e558ic7dyu@#3'
+# ORIGINAL SECRET KEY
+# SECRET_KEY = 'django-insecure-ii#n*gcw_(ps7xacbuq&+hp0@#n&(_-jy69z7e558ic7dyu@#3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Original DEBUG SETTING
+# DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
