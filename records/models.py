c02f673f3d9db5b28428389df2176740a565fd95
Lewey <dlmelchor12@gmail.com>
2023-07-19 20:48:42 -0700

    ready to start changes

--- records/models.py
+++ records/models.py
@@ -21,7 +21,7 @@ class Record(models.Model):
     album = models.CharField(max_length=125, null=True)
     artist= models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="records", null=True)
     artwork = models.URLField(null=True, blank=True, max_length=300)
-    attachment = models.FileField()
+    attachment = models.FileField(null=True, blank=True)
     date = models.DateField(auto_now_add=False, blank=True, null=True)
     price= models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     owner = models.ForeignKey(USER_MODEL, related_name="record_owner", on_delete=models.CASCADE, default=1, null=True)
